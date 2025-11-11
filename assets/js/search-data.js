20b73b623ece4667f0572ca3efb8a6358c51b71f
llllkkkk0 <159040310+llllkkkk0@users.noreply.github.com>
2025-11-11 01:25:04 +0000

    Deploying to gh-pages from @ llllkkkk0/kexinlin@5e623ba8045340f4cdf7f32cb8b3ba717cbd7d79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -102,7 +102,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%6C%69%6E%35@%77%70%69.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -112,25 +112,32 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/kexinlin/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/kexin-lin-b6622a2b5", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0009-3713-2537", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=Pp72RqEAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("", "_blank");
         },
       },{
       id: 'light-theme',
